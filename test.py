2d74a28d3d8a1a39e27b8998d0bf0bf2c687102d
Abhinav Khot <23110006@iitgn.ac.in>
2025-08-15 22:11:01 +0530

    fix formatting issues

--- test.py
+++ test.py
@@ -6,27 +6,26 @@ nos = [0] * 26
 for x in s:
     nos[ord(x) - ord('A')] += 1
 
-prev = 'a'
+Prev = 'a'
 ans = []
 
-for i in range(len(s)): 
-    possible = "" 
-    found = False
+for i in range(len(s)):
+    Possible = ""
+    Found = False
     for j in range(26):
-        if (not found and UP[j] != prev and nos[j] > 0):
-            possible = UP[j]
-            found = True
-        
-        if (nos[j] > (len(s) - i) // 2): #will become imposi
-            possible = UP[j]
+        if (not Found and UP[j] != Prev and nos[j] > 0):
+            Possible = UP[j]
+            Found = True
+        if nos[j] > (len(s) - i) // 2:  # will become imposi
+            Possible = UP[j]
 
-    if possible == "":
+    if Possible == "":
         print(-1)
         break
 
-    ans.append(possible)
-    nos[ord(possible) - ord('A')] -= 1
-    prev = possible
+    ans.append(Possible)
+    nos[ord(Possible) - ord('A')] -= 1
+    Prev = Possible
 else:
     for i in range(len(s) - 1):
         if ans[i] == ans[i + 1]:
