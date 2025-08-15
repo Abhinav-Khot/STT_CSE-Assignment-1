d20edcd700ea65e27dc8c5588ee9bae0e43ce4aa
Abhinav Khot <23110006@iitgn.ac.in>
2025-08-15 22:03:07 +0530

    Add python file and the python workflow

---
+++ test.py
@@ -0,0 +1,36 @@
+from string import ascii_uppercase as UP
+s = input()
+
+nos = [0] * 26
+
+for x in s:
+    nos[ord(x) - ord('A')] += 1
+
+prev = 'a'
+ans = []
+
+for i in range(len(s)): 
+    possible = "" 
+    found = False
+    for j in range(26):
+        if (not found and UP[j] != prev and nos[j] > 0):
+            possible = UP[j]
+            found = True
+        
+        if (nos[j] > (len(s) - i) // 2): #will become imposi
+            possible = UP[j]
+
+    if possible == "":
+        print(-1)
+        break
+
+    ans.append(possible)
+    nos[ord(possible) - ord('A')] -= 1
+    prev = possible
+else:
+    for i in range(len(s) - 1):
+        if ans[i] == ans[i + 1]:
+            print(-1)
+            break
+    else:
+        print("".join(ans))
